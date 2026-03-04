cfd515e95be47357b2fc824e17451a0281f45a76
Mateen Mushrif <mateenmushrif04@gmail.com>
2026-03-04 15:18:06 +0530

    fix: use railway dynamic port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { startClickFlusher } from "./services/clickBuffer";
 
-const PORT: number = 4000;
+const PORT: number = Number(process.env.PORT) || 4000;
 
 app.listen(PORT, () => {
     console.log(`Backend server is running on http://localhost:${PORT}`);
